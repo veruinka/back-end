3672c8c11ccb19864edfac2ee951afa9876a451b
veruinka <verkastudentka@gmail.com>
2021-07-13 20:06:13 +0300

    13 ijulia

--- node/hello.js
+++ node/hello.js
@@ -1,1 +1,15 @@
 console.log('Hello, World!');
+
+var text = 'Labas "vau" \'faina\' oga \ua4c3 va taip\nva';
+console.log(text);
+
+var sk = 10;
+var sk1 = 5;
+var rez = sk + text;
+console.log(rez);
+
+rez = sk + sk1 + text;
+console.log(rez);
+
+rez = text + sk + sk1;
+console.log(rez);
