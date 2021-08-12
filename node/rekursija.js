e0915b096578e1952175f637f305c084ce4c5ebb
veruinka <verkastudentka@gmail.com>
2021-08-12 16:23:09 +0300

    12 avgusta

--- node/rekursija.js
+++ node/rekursija.js
@@ -22,13 +22,16 @@
 
 
 function faktorialas(sk) {
+    if (typeof sk !== 'number') {
+        throw new Error('Must be number'); //prevencija nuo debilu
+    };
     if (sk < 0) {
         throw new Error('Must be 0 or positive'); //prevencija nuo nuamzinimo
-    }
+    };
     if (sk === 0) {
         return 1;
-    }
+    };
     return sk * faktorialas(sk - 1);
 }
 
-console.log(faktorialas(-3)); 
+console.log(faktorialas(undefined)); 
